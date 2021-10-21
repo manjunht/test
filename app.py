50e133a441a571ea8c0f8446ae18250ba799fdc7
manjunht <42174660+manjunht@users.noreply.github.com>
2021-10-21 20:02:52 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import
+os
+from flask import Flask
+app = Flask(__name__)
+…
+…
+color =
+"
+app.route
+def main
+print (
+return render_template (('hello. html', color =
+if __name__ == "__
+app.run host ==" 0.0.0.0", port =="
